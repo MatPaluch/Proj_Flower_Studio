36460e8791fbb7c78475134578f7eb9ffee607c9
Piotr Frontczak <piotrf2023@gmail.com>
2024-01-30 19:52:07 +0100

    2nd modal window added

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    form: document.querySelector("[data-modal-form]"),
   };
 
   function toggleModal() {
@@ -12,5 +13,23 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  
-})(); 
+  // drugie okno modalne
+
+  const refs2 = {
+    openModal2Btn: document.querySelector("[data-modal-open-2]"),
+    closeModal2Btn: document.querySelector("[data-modal-close-2]"),
+    modal: document.querySelector("[data-modal-2]"),
+  };
+
+  function thankOrder(e) {
+    e.preventDefault();
+    refs.modal.classList.toggle("is-hidden");
+    refs2.modal.classList.toggle("is-hidden");
+  }
+
+  function closeModalThanks() {
+    refs2.modal.classList.add("is-hidden");
+  }
+  refs.form.addEventListener("submit", (e) => thankOrder(e));
+  refs2.closeModal2Btn.addEventListener("click", closeModalThanks);
+})();
