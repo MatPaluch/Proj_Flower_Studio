5bdbf4cf468bdfa80028ea6388df389a2a6ed7b3
MatPaluch <xp.mateusz@gmail.com>
2024-01-31 09:15:13 +0100

    Finish

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,33 +1,35 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    form: document.querySelector("[data-modal-form]"),
+    openModal3Btn: document.querySelector('[data-modal-open-3]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    form: document.querySelector('[data-modal-form]'),
   };
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModal3Btn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   const refs2 = {
-    openModal2Btn: document.querySelector("[data-modal-open-2]"),
-    closeModal2Btn: document.querySelector("[data-modal-close-2]"),
-    modal: document.querySelector("[data-modal-2]"),
+    openModal2Btn: document.querySelector('[data-modal-open-2]'),
+    closeModal2Btn: document.querySelector('[data-modal-close-2]'),
+    modal: document.querySelector('[data-modal-2]'),
   };
 
   function thankOrder(e) {
     e.preventDefault();
-    refs.modal.classList.toggle("is-hidden");
-    refs2.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+    refs2.modal.classList.toggle('is-hidden');
   }
 
   function closeModalThanks() {
-    refs2.modal.classList.add("is-hidden");
+    refs2.modal.classList.add('is-hidden');
   }
-  refs.form.addEventListener("submit", (e) => thankOrder(e));
-  refs2.closeModal2Btn.addEventListener("click", closeModalThanks);
+  refs.form.addEventListener('submit', e => thankOrder(e));
+  refs2.closeModal2Btn.addEventListener('click', closeModalThanks);
 })();
